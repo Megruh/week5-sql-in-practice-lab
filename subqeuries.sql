0fbf647bf4022f1cbd3140d34b9b80f9143e8e58
Meg McCauley <memi.mc14@gmail.com>
2022-02-16 16:28:06 -0700

    finished week5 sql in practice lab

---
+++ subqeuries.sql
@@ -0,0 +1,33 @@
+SELECT * FROM invoice
+WHERE invoice_id IN (
+  SELECT invoice_id FROM invoice_line
+  WHERE unit_price > 0.99);
+
+SELECT * FROM playlist_track
+WHERE playlist_id IN (
+SELECT playlist_id FROM playlist
+WHERE name = 'Music');
+
+SELECT * FROM track
+WHERE track_id IN (
+SELECT track_id FROM playlist_track
+WHERE playlist_id = 5);
+
+SELECT * FROM track
+WHERE genre_id IN (
+SELECT genre_id FROM genre
+WHERE genre_id = 22);
+
+SELECT * FROM track
+WHERE album_id IN (
+SELECT album_id FROM album
+WHERE album_id = 60);
+
+SELECT * FROM track
+WHERE album_id IN(
+	SELECT album_id FROM album
+	WHERE artist_id IN (
+		SELECT artist_id FROM artist
+		WHERE artist_id = 51
+  )
+);
